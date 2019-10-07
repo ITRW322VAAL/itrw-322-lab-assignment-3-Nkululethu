a01836ef233efe217b044280a4c821b5befd8b1a
Amanda <freedoma.mkhize@gmail.com>
2019-10-07 16:36:10 +0200

    26720175 - updated ITRW322 html file

---
+++ sockets.py
@@ -0,0 +1,8 @@
+ITRW 322 Networks Assignment
+
+Programming Sockets in Python
+
+A socket is one endpoint of a two-way communication
+link between two programs running on the network.
+A socket is bound to a port number so that the TCP layer
+can identify the application that data is destined to be sent to.
